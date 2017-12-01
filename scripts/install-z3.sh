29f6d7a71b16f5a20f2a8cf667337f12cc3babd0
Shiyou Huang <hsyhust@gmail.com>
2017-12-01 16:48:00 -0600

    Update install-z3.sh

--- scripts/install-z3.sh
+++ scripts/install-z3.sh
@@ -9,5 +9,5 @@
 # mkdir -p ~/.local/lib
 wget https://github.com/Z3Prover/z3/releases/download/z3-4.5.0/z3-4.5.0-x64-ubuntu-14.04.zip
 unzip z3-4.5.0-x64-ubuntu-14.04.zip
-cp z3-4.5.0-x64-ubuntu-14.04/bin/z3 /usr/local/bin
-cp z3-4.5.0-x64-ubuntu-14.04/bin/*.so /usr/local/lib
+cp z3-4.5.0-x64-ubuntu-14.04/bin/z3 ~/.local/bin
+cp z3-4.5.0-x64-ubuntu-14.04/bin/*.so ~/.local/lib
